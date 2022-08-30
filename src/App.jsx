52b74ac31777941128bcbd70b6b536343dbc0092
Diego Aldao <diego_ind@live.com.ar>
2022-08-30 12:21:44 -0300

    añadido react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Contenedor from "./components/Contenedor";
-import Navbar from "./components/Navbar";
+import Detalle from "./components/Detalle";
+import Layout from "./layouts/Layout";
+import { useState } from "react";
 
 function App() {
+  const [pais, setPais] = useState("");
   return (
     <>
-      <Navbar />
-      <Contenedor />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Contenedor setPais={setPais} />
+            </Layout>
+          }
+        />
+        <Route
+          path="/detalle"
+          element={
+            <Layout>
+              <Detalle pais={pais} />
+            </Layout>
+          }
+        />
+      </Routes>
     </>
   );
 }
